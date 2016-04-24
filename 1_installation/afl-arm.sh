c9bea5ceaca20d03915aaee3e39dd13993a69a18
floyd-fuh <floyd@floyd.ch>
2016-04-24 09:46:15 +0200

    Fixed some variable renaming that was done in AFL

--- 1_installation/afl-arm.sh
+++ 1_installation/afl-arm.sh
@@ -8,13 +8,13 @@ echo `find . -type d -iname "afl-*"|sort|tail -1`
 cd `find . -type d -iname "afl-*"|sort|tail -1`
 export CC=clang
 export CXX=clang++
-export AFL_NOX86=1
+export AFL_NO_X86=1
 make
 cd llvm_mode
 make
 cd ..
 echo "Provide sudo password for sudo make install"
-sudo AFL_NOX86=1 make install
+sudo AFL_NO_X86=1 make install
 sudo rm /usr/local/bin/afl-clang
 sudo rm /usr/local/bin/afl-clang++
 sudo ln -s /usr/local/bin/afl-clang-fast /usr/local/bin/afl-clang
